807e95e496b6012e138ca18adaae2e142d64d046
Tararatel <caratel18@mail.ru>
2021-08-18 19:42:19 +0300

    Lesson 7

---
+++ Lesson_7/.eslintrc.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+};
